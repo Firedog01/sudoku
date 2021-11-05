88eab29362d3a7a1ccc34fef8d960ecbc2886e6d
LiptonLeon <krystian.baraniecki@gmail.com>
2021-11-05 20:52:42 +0100

    Wstępna implementacja SudokuField

--- src/main/java/SudokuField.java
+++ src/main/java/SudokuField.java
@@ -1,4 +1,11 @@
 public class SudokuField{
-    private SudokuElement element;
+    private int value;
+
     private SudokuBoard board;
+    private SudokuRow row;
+    private SudokuColumn column;
+    private SudokuBox box;
+
+    public int getFieldValue(){ return value; }
+    public void setFieldValue(int value){ this.value = value; }
 }
